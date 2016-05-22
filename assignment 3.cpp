5cffa7798fd322199662777b55d608ca7de16bae
sanawarhussian <sanawar.hussain18@gmail.com>
2016-05-22 23:18:57 +0500

    Update assignment 3.cpp

--- assignment 3.cpp
+++ assignment 3.cpp
@@ -5,55 +5,87 @@
 //#include <Lego>
 //#include <projects>
 //#include <friendzoned>
-//#disinclude <happiness>
-//#disinclude <free_time>
+//#exclude <happiness>
+//#exclude <free_time>
  using namespace std;
- string Title = "Story of ma' life";
+ string Title = "Story of my life";
  string Protagonist = "Snawar Hussain";
  string Gender = "Male";
  string profession = "Engineering ";
  const int current_age =20;
+ const bool Single = true;
  int age = 20;
  int death_age =70 ;
- const bool Single = true;
+ 
+
+ class CEME
+ {
+ public:
+ void Departments_of_EME()
+ {
+  string ME_department = "Mardan Department";
+  string CE_department= "Dabba Department ";
+  string EE_department= "Civil Hospital";
+  string MTS_department = "Diploma Department";
+ }
+ 
+ void setmydepartment(string name)
+ {
+ de_name = name;
+ }
+
+ string getmydepartment()
+ {
+ return de_name;
+ }
+ 
+ void display_de()
+ {
+ cout << "Now your are in "<<getmydepartment()<< "Department of CEME"<<endl;
  
  
- class Girlfriend
-{
-public:
+ }
 
-	Girlfriend(string name)
-	{
-	setgalname(name);
-	}
-	void setgalname(string name)
-	{
-	galname = name;
-	}
-	string getgalname()
-	{
-	
-	return galname;
-	
-	}
-	void galproperties(  ){
+ void life_in_MTS()
 
-		cout << "an imaginary gairfirend don't have any physical properties!! \n Let your imagination get the best outta'you  "<<endl; 
-      	}
+ {
+ cout << "Being a freshmen at Department of Mechatronics Engineering  of NUST and a keen observer,"<<endl; 
+cout << "it would not be wrong when I put with complete honesty that you find this place hovered with threats,"<<endl;
+cout << "threats of signing up for a time of four years full of threats, and when passing out you look out for"<<endl; 
+	 cout << "careers and figure out it is a degree full of threat.I remember my first day in this department and being"<<endl; 
+	 cout << "in face with the first threat. Having heard already that worst ragging is authorized and registered in the history of this department,"<<endl;
+	 cout << "I thought that I was ready for what was coming. Just after the first lecture by Prof. M.A Khan,"<<endl;
+	 cout << "guys from senior degrees were waiting for us."<<endl; 
+	 cout << "Girls were eliminated from the class as ‘x’ is eliminated from an algebraic problem and then all"<<endl;
+	 cout << "was between ourselves, seniors, desks, benches ,our ties and belts. We were forewarned that the seniors here, in"<<endl;
+	 cout << "terms of being unforgettable, shock giving capacity and recurring the shock waves, are a lot like Earthquake of 2005 ,"<<endl; 
+	 cout << "the only difference was despite of millions this one inflicted lives of only 78. It should be said that after a complicated"<<endl; 
+	 cout << "interaction of two weeks, we were able to breathe. Yet on an advisory note, College Library is a useful place if one wants to avoid"<<endl; 
+	 cout << "such #comedy night like interactions"<<endl;
+ 
+ }
 
-	void displaysympathy ()
-	{
-	
-	cout << " Single as ever, you are useless , better stay in frienzone or stick to your imagination! " <<endl;
-	}
+ void Project_in_MTS()
+ {
+	 cout <<"Next comes the life of so called damned projects. This one I would personally put as the threat of #Khuwari."<<endl; 
+	  cout <<"In a session with seniors, we were forewarned that Projects here are gonna take your lives. I realized the truth of the"<<endl;
+	  cout <<"premonition when once on coming from MTS department at 14:00, I heard from a senior: “Itni jaldi tum kaise aa gye agar MTS me ho.?"<<endl; 
+	  cout <<"Beta G Abhi to time aaye ga jb raat 11 bje wapis aao ge and Finals me b projects kr rahey ho ge.”"<<endl;
+ cout <<"Generally, wishes don’t come true that easy. But this wish of that senior got the epic in 2nd semester."<<endl; 
+ cout <<"In short  #LEGO ne saari kasar poori ker dee.  The foretoken though, that to date tortures every soul at Mechatronics Department is"<<endl; 
+ cout <<"that we are doing #Diploma."<<endl;
 
+ cout <<"                               Dil me Engineer bn’ney k sapney sjaye jab b pakra solder"<<endl;
+ cout <<"To jaana is bezaar dil ne K dont forget U R a diploma Holder"<<endl;
 
-private : 
+ cout <<"This word was taken so seriously by a degree mate that he asked the Professor ,”Sir! kya Hum Diploma ker rahey hein..??”"<<endl;
+ }
+ private :
+	string de_name;
 
-	string galname ;
-   };
+ };
 
- class Snawar_Hussain  
+class Snawar_Hussain  
  
  {
  
@@ -117,36 +149,23 @@ return false ;
  {
    return true;
  }
+
+
+
 int main ()
 
 
-{
+{              
+	
+	CEME EME;
+
 				bool peace_in_life;
 				bool nigga_is_dead_now ;
 				bool alive ;		//theoratically true
-				string  gal_name ;
 				string responce ;
-		Girlfriend gal("imagination");
-
-		cout <<" current you have a girlfreind named "<<gal.getgalname()<<endl;
-		cout << "DUDE  do you even  have a real girlfriend y/n  ? " <<endl;
-		getline(cin, responce );
 		
- if (responce == "y")
-	{
-			cout << " your are lyin'nigga' don't fool me .. " <<endl;
-			
-					
- }
- else if (responce == "n")
- {
-	 gal.displaysympathy();
- 
- }
- else 
- {
- cout << "Dont talk gibberish, just y for  Yes and n for No, ain't no rocket scinece here "<<endl; 
- }
+
+	
 
  while (age < death_age)
 	{ alive = true ;
@@ -158,7 +177,7 @@ int main ()
  {
 
 	 peace_in_life = false ;
-     do_engineering ();
+     	 do_engineering ();
 	 get_a_girlfriend();
 	 get_job();
 	 get_married_to_cousin(); 
@@ -176,15 +195,53 @@ if ( age >= death_age)
 {
   nigga_is_dead_now = true ;
 }
+char responce_1;
+char responce_2;
+string DE;
+cout << "From which Department of CEME you belong ? "<<endl; 
+getline(cin , DE);
+EME.setmydepartment(DE);
+EME.display_de();
+cout << "press k to read protagonist life as Dimploma holder and as a FIrst year in MTS CEME"<<endl;
+cin>>responce_1;
+if (responce_1 == 'k')
 
-cout << "SO here is the story of"<<Protagonist<<"'s life in a Nut Shell "<<endl;
+{
+	EME.life_in_MTS();
+}
+else 
+{
+
+cout << "Not interested ? , ME niether "<<endl;
+
+}
+cout << "press y to read protagonist work on his Projects"<<endl;
+cin>>responce_2;
+if (responce_2 == 'y')
+{
+
+EME.Project_in_MTS();
+
+}
+else 
+{
+
+cout << "Not interested ? , Meh , let's move on  "<<endl;
+
+}
+
+
+	cout << "SO here is the story of"<<Protagonist<<"'s life in a Nut Shell "<<endl;
 
 cout << "age					"<<current_age<<endl;
 cout << "Gender					"<< Gender<<endl;
 cout <<"Being Single				"<<Single<<endl; 
 cout << "profession				"<<profession<<endl;
 cout << "gain from Engineering			"<<do_engineering()<<endl; 
-
-
+cout << "x===========DEAD(x_x)DEAD==========x"<<endl;
 }
+
+
+
+
 
